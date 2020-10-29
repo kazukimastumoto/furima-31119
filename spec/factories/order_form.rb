0a689c4e79324737d54062593e2265ac50c8fb9e
kazukimastumoto <uver.loveloveleke@icloud.com>
2020-10-29 15:35:49 +0900

    Formオブジェクトモデル単体テストコードの実施

---
+++ spec/factories/order_form.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :order_form do
+    postal_code      { 111-1111 }
+    prefectures_id   { 2 }
+    city             { "船橋市" }
+    address          {"3丁目"}
+    cellphone_number { "08054643873" }
+  end
+end
---
+++ spec/models/order_form_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe OrderForm, type: :model do
+  describe '購入情報の保存' do
+    before do
+      @order_form = FactoryBot.build(:order_form)
+    end
+
+      it '郵便番号がなければ購入できない' do
+        @order_form.postal_code = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Postal code is invalid")
+      end
+
+      it '郵便番号にハイフンがないと購入できない' do
+        @order_form.postal_code = "1111111"
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Postal code is invalid")
+      end
+
+      it '都道府県の情報がないと購入できない' do
+        @order_form.prefectures_id = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Prefectures is not a number")
+      end
+
+      it '都道府県の情報1では購入できない' do
+        @order_form.prefectures_id = 1
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Prefectures must be other than 1")
+      end
+
+      it '市町村区の情報がないと購入できないこと' do
+      @order_form.city = ""
+      @order_form.valid?
+      expect(@order_form.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '番地の情報がないと購入できないこと' do
+        @order_form.address = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Address can't be blank")
+      end
+
+      it '電話番号がないと購入できないこと' do
+        @order_form.cellphone_number = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Cellphone number can't be blank")
+      end
+
+      it '電話番番号にハイフンがあり、かつ11桁以上だと購入できないこと' do
+        @order_form.cellphone_number = "080-5111-5111"
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Cellphone number is invalid")
+      end
+  end
+end
